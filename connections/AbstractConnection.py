ce3593eeec328d8dad215fb99131e5f3233e8b57
zadjii <zadjii@gmail.com>
2015-11-06 18:16:35 -0800

    Start connection stuff.

---
+++ connections/AbstractConnection.py
@@ -0,0 +1,66 @@
+__author__ = 'Mike'
+import abc
+
+
+class AbstractConnection(object):
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def send_obj(self, message_obj):
+        """
+        Serializes the provided message, and sends it along the connection.
+        """
+        return
+
+    @abc.abstractmethod
+    def recv_obj(self):
+        """
+        Retrieves a serialized object from whatever the input stream is, and
+        converts it to a Message object. If there is any file data coming with
+        the message, this will need to retrieve it and pack it into the message
+        as well. OR provide a mechanism for reading the data piece by piece.
+
+        I don't really know how websocket file transfer is going to work yet, so
+        maybe do that FIRST
+        """
+        # fixme ^^^^ the part about ws:// file transfer.
+        return
+
+    @abc.abstractmethod
+    def recv_next_data(self, length):
+        """
+        Retrieves the next data[length] from this connection.
+
+        So, a websock will keep grabing messages until length == length
+        but what if one message gets you nBytes > length??
+            THEN ITS PROBABLY BAD.
+
+        :param length: length of the data to return. Might be a little more?
+        :return:
+        """
+        return
+
+    @abc.abstractmethod
+    def send_next_data(self, data):
+        """
+        Sends the specified data down the connection. It just does it.
+        No regard for what the data is or how it happens.
+        :param data: data to send
+        :return:
+        """
+        return
+
+
+"""
+    OKAY SO HERES THE DEAL.
+    Whenever we would be retrieving file data, we always go:
+    "What's the message? oh cool, length of file data, lemme just
+      while loop to pull the data."
+    So, now its basically the same.
+    We'll recv_obj -> see that there is file data coming,
+      and while loop to pull it all down.
+    And similar for the sending of files. Very #fuckitshipit.
+"""
+
+
+
--- messages/MessageDeserializer.py
+++ messages/MessageDeserializer.py
@@ -8,36 +8,36 @@ _decoder_table = {
     , AUTH_ERROR: -2
     , GENERIC_ERROR: -1
     , NEW_HOST_MSG: NewHostMessage.deserialize # 0
-    , ASSIGN_HOST_ID: AssignHostIDMessage.deserialize #1
-    , HOST_HANDSHAKE: 2
+    , ASSIGN_HOST_ID: AssignHostIDMessage.deserialize # 1
+    , HOST_HANDSHAKE: HostHandshakeMessage.deserialize # 2
     , REMOTE_HANDSHAKE: 3
     , REM_HANDSHAKE_GO_FETCH: 4
-    , REQUEST_CLOUD: 5
-    , GO_RETRIEVE_HERE: 6
-    , PREPARE_FOR_FETCH: 7
-    , HOST_HOST_FETCH: 8
-    , HOST_FILE_TRANSFER: 9
+    , REQUEST_CLOUD: RequestCloudMessage.deserialize # 5
+    , GO_RETRIEVE_HERE: GoRetrieveMessage.deserialize # 6
+    , PREPARE_FOR_FETCH: PrepareForFetchMessage.deserialize # 7
+    , HOST_HOST_FETCH: HostHostFetchMessage.deserialize # 8
+    , HOST_FILE_TRANSFER: HostFileTransferMessage.deserialize # 9
     , MAKE_CLOUD_REQUEST: 10
     , MAKE_CLOUD_RESPONSE: 11
     , MAKE_USER_REQUEST: 12
     , MAKE_USER_RESPONSE: 13
-    , MIRRORING_COMPLETE: 14
-    , GET_HOSTS_REQUEST: 15
-    , GET_HOSTS_RESPONSE: 16
+    , MIRRORING_COMPLETE: MirroringCompleteMessage.deserialize # 14
+    , GET_HOSTS_REQUEST: GetHostsRequestMessage.deserialize # 15
+    , GET_HOSTS_RESPONSE: GetHostsResponseMessage.deserialize # 16
     , COME_FETCH: 17
-    , REMOVE_FILE: 18
-    , HOST_FILE_PUSH: 19
-    , STAT_FILE_REQUEST: 20
-    , STAT_FILE_RESPONSE: 21
-    , LIST_FILES_REQUEST: 22
-    , LIST_FILES_RESPONSE: BaseMessage.deserialize # 23
+    , REMOVE_FILE: RemoveFileMessage.deserialize # 18
+    , HOST_FILE_PUSH: HostFilePushMessage.deserialize # 19
+    , STAT_FILE_REQUEST: StatFileRequestMessage.deserialize # 20
+    , STAT_FILE_RESPONSE: StatFileResponseMessage.deserialize # 21
+    , LIST_FILES_REQUEST: ListFilesRequestMessage.deserialize # 22
+    , LIST_FILES_RESPONSE: ListFilesResponseMessage.deserialize # 23
     , READ_FILE_REQUEST: 24
     , READ_FILE_RESPONSE: 25
-    , CLIENT_SESSION_REQUEST: 26
-    , CLIENT_SESSION_ALERT: 27
-    , CLIENT_SESSION_RESPONSE: 28
-    , CLIENT_FILE_PUT: 29
-    , CLIENT_FILE_TRANSFER: 30
+    , CLIENT_SESSION_REQUEST: ClientSessionRequestMessage.deserialize # 26
+    , CLIENT_SESSION_ALERT: ClientSessionAlertMessage.deserialize # 27
+    , CLIENT_SESSION_RESPONSE: ClientSessionResponseMessage.deserialize #28
+    , CLIENT_FILE_PUT: ClientFilePutMessage.deserialize # 29
+    , CLIENT_FILE_TRANSFER: ClientFileTransferMessage.deserialize #30
 }
 
 
