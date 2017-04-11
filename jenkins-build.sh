99c056557a6887f8f70cde84a51966394f37ebcf
zadjii <zadjii@gmail.com>
2017-04-11 08:09:41 -0700

    This script was still wrong.

--- jenkins-build.sh
+++ jenkins-build.sh
@@ -4,7 +4,8 @@ python nebr.py kill -i jenkins
 python nebr.py -i jenkins
 rm ./instances/remote/jenkins/nebr.conf
 
-echo "PORT = 23457\nWSPORT = 34568" > ./instances/jenkins/remote/nebr.conf
+# This is for nebs, not nebr
+# echo "PORT = 23457\nWSPORT = 34568" > ./instances/remote/jenkins/nebr.conf
 
 python nebr.py migrate -i jenkins
 python nebr.py start -i jenkins 
