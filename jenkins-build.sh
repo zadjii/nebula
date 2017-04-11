8590013186810504ce1ac4e9e4c34578d5a41246
zadjii <zadjii@gmail.com>
2017-04-11 08:15:10 -0700

    Come on, lets go already
    For some reason it's not getting the args? I'm not sure.

--- jenkins-build.sh
+++ jenkins-build.sh
@@ -6,6 +6,8 @@ rm ./instances/remote/jenkins/nebr.conf
 
 # This is for nebs, not nebr
 # echo "PORT = 23457\nWSPORT = 34568" > ./instances/remote/jenkins/nebr.conf
-
+echo on
 python nebr.py migrate -i jenkins
+echo "Migrated old DB"
 python nebr.py start -i jenkins 
+echo "Started nebr"
